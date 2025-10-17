fc8fb9e2cdeff56f872d38783d7be16eb7209456
fridi <fridi.beims@gmail.com>
2025-10-17 22:38:42 +0200

    Add API call to fetch live game data

--- tools.py
+++ tools.py
@@ -1,3 +1,7 @@
 import requests
 
-api_url = "https://jsonplaceholder.typicode.com/todos/1"
+api_url_allgamedata = "https://127.0.0.1:2999/liveclientdata/allgamedata"
+
+respone = requests.get(api_url_allgamedata, verify=False)
+data = respone.json()['allPlayers']
+print(data)
